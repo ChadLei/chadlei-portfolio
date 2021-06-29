f69977d1aa40fd831d110a8feeb3cc0af524aeca
chadlei <cblei@uci.edu>
2021-06-28 21:39:58 -0700

    remove ig

--- src/hooks/use-instagram.js
+++ src/hooks/use-instagram.js
@@ -1,39 +1,39 @@
-import {graphql, useStaticQuery} from 'gatsby'
-
-const useInstagram = () => {
-    // Fields to sort by:
-    // id
-    // likes
-    // comments
-    // mediaType
-    // preview
-    // timestamp
-    // caption
-    const data = useStaticQuery(graphql`
-    query {
-        allInstaNode(limit:6, sort: { fields: timestamp, order: DESC }) {
-         nodes {
-           id
-           username
-           caption
-           localFile {
-             childImageSharp{
-               fluid(quality: 50) {
-                 ...GatsbyImageSharpFluid_withWebp
-               }
-             }
-           }
-         }
-       }
-       }
-    `)
-
-    return data.allInstaNode.nodes.map(node => ({
-        ...node.localFile.childImageSharp,
-        id: node.id,
-        caption: node.caption,
-        username: node.username
-    }))
-}
-
-export default useInstagram
+// import {graphql, useStaticQuery} from 'gatsby'
+//
+// const useInstagram = () => {
+//     // Fields to sort by:
+//     // id
+//     // likes
+//     // comments
+//     // mediaType
+//     // preview
+//     // timestamp
+//     // caption
+//     const data = useStaticQuery(graphql`
+//     query {
+//         allInstaNode(limit:6, sort: { fields: timestamp, order: DESC }) {
+//          nodes {
+//            id
+//            username
+//            caption
+//            localFile {
+//              childImageSharp{
+//                fluid(quality: 50) {
+//                  ...GatsbyImageSharpFluid_withWebp
+//                }
+//              }
+//            }
+//          }
+//        }
+//        }
+//     `)
+//
+//     return data.allInstaNode.nodes.map(node => ({
+//         ...node.localFile.childImageSharp,
+//         id: node.id,
+//         caption: node.caption,
+//         username: node.username
+//     }))
+// }
+//
+// export default useInstagram
